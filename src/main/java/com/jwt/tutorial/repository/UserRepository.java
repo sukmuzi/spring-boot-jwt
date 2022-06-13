38d36650492244e194d8a91e09d92c13eb9f4732
sukmuzi <sukmuzi@naver.com>
2022-06-13 17:49:33 +0900

    user, token, login dto 및 repository 추가

---
+++ src/main/java/com/jwt/tutorial/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jwt.tutorial.repository;
+
+import com.jwt.tutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username); // username 을 기준으로 user 정보를 가져올 때 권한 정보도 같이 가져옴.
+}
