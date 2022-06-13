2f727f7ddd6b03856f9cd5c3c0b84b33f1a868d5
sukmuzi <sukmuzi@naver.com>
2022-06-13 17:28:55 +0900

    init

---
+++ src/main/java/com/jwt/tutorial/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.jwt.tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.jwt.tutorial.jwt.JwtAccessDeniedHandler;
+import com.jwt.tutorial.jwt.JwtAuthenticationEntryPoint;
+import com.jwt.tutorial.jwt.JwtSecurityConfig;
+import com.jwt.tutorial.jwt.TokenProvider;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final TokenProvider tokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	
+	public SecurityConfig(
+			TokenProvider tokenProvider,
+			JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+			JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+		
+		this.tokenProvider = tokenProvider;
+		this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+		this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		   .antMatchers(
+				   "/h2-console/**"
+				   , "/favicon.ico");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.exceptionHandling()
+			.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+			.accessDeniedHandler(jwtAccessDeniedHandler)
+			
+			.and()
+			.headers()
+			.frameOptions()
+			.sameOrigin()
+			
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			
+			.and()
+			.authorizeRequests()
+			.antMatchers("/api/hello").permitAll()
+			.antMatchers("/api/authenticate").permitAll()
+			.antMatchers("/api/signup").permitAll()
+			.anyRequest().authenticated()
+		
+			.and()
+			.apply(new JwtSecurityConfig(tokenProvider));
+	}
+}
