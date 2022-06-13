37a75d2880b7492369fc769006c27cc343c44387
sukmuzi <sukmuzi@naver.com>
2022-06-13 18:11:29 +0900

    CustomUserDetailsService 추가

--- src/main/java/com/jwt/tutorial/repository/UserRepository.java
+++ src/main/java/com/jwt/tutorial/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @EntityGraph(attributePaths = "authorities")
+    @EntityGraph(attributePaths = "authorities") // 쿼리가 수행될 때 Lazy 조회가 아니고 Eager 조회로 authorities 정보를 같이 가져옴.
     Optional<User> findOneWithAuthoritiesByUsername(String username); // username 을 기준으로 user 정보를 가져올 때 권한 정보도 같이 가져옴.
 }
