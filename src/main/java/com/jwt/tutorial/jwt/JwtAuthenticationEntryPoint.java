11b7f9149d2551f67596657a8272ad8dbcff3796
sukmuzi <sukmuzi@naver.com>
2022-06-14 16:49:45 +0900

    @Component 어노테이션 추가

--- src/main/java/com/jwt/tutorial/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/jwt/tutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -8,14 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);;
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 
 }
